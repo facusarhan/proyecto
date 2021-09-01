b90fb8145c40a19a41308eeb68d814733be12476
facundo sarhan <facu.sarhan2000@gmail.com>
2021-09-01 19:51:32 -0300

    js mejorada 2

---
+++ Scripts/listaPc's.js
@@ -0,0 +1,42 @@
+const listaPcs = [
+    {
+        tipo: "gamer",
+        precio: 63000,
+        componentes: "Ryzen 5 G, integrada, 8gb ram, 512gb ssd"
+    },
+    {
+        tipo: "gamer",
+        precio: 150000,
+        componentes: "Ryzen 5, rx 570 8gb, 16gb ram, 240gb ssd" 
+    },
+    {
+        tipo: "gamer",
+        precio: 200000,
+        componentes: "intel i5, 1660 super, 16gb ram, 480gb ssd"
+    },
+    {
+        tipo: "gamer",
+        precio: 250000,
+        componentes: "Ryzen 7, Rtx 3070, 16gb ram, 1tb nvme"
+    },
+    {
+        tipo: "edicion",
+        precio: 180000,
+        componentes: "Ryzen 5, 1660 super, 16gb ram, 240gb ssd"
+    },
+    {
+        tipo: "edicion",
+        precio: 230000,
+        componentes: "Ryzen 5, Rtx 3060ti, 16gb ram, 4480gb ssd"
+    },
+    {
+        tipo: "edicion",
+        precio: 480000,
+        componentes: "Ryzen 7, Rtx 3080, 32gb ram, 1tb nvme"
+    },
+    {
+        tipo: "estudiante",
+        precio: 50000,
+        componentes: "Ryzen 3 G, integrada, 8gb ram, 480gb ssd"
+    },
+];
--- Scripts/recomendaciones.js
+++ Scripts/recomendaciones.js
@@ -1,50 +1,49 @@
-class Computadora {
-    constructor(tipo, precio){
-        this.tipo = tipo;
-        this.precio = precio;
-    }
-    recomendar(){
-        if (this.tipo = "gamer" && this.precio < 100000){
-            alert("te recomiendo un ryzen 5 o Athlon 3000G con graficos integrados y en un futuro poner una grafica dedicada");
-            console.log("te recomiendo un ryzen 5 o Athlon 3000G con graficos integrados y en un futuro poner una grafica dedicada");
+let cuerpoTabla = document.getElementById("cuerpo_tabla");
 
-        } else if (this.tipo = "gamer" && this.precio > 100000 && this.precio < 150000){
-            alert("te recomiendo un Ryzen 5 o Intel Core I5 con una grafica gama media/baja");
-            console.log("te recomiendo un Ryzen 5 o Intel Core I5 con una grafica gama media/baja");
-
-        } else if (this.tipo = "gamer" && this.precio > 150000 && this.precio < 200000){
-            alert("te recomiendo un Ryzen 5 o Intel Core I5 con una grafica gama media");
-            console.log("te recomiendo un Ryzen 5 o Intel Core I5 con una grafica gama media");
-
-        } else if (this.tipo = "gamer" && this.precio > 200000){
-            alert("te recomiendo un Ryzen 7 o Intel Core I7 y una grafica gama media/alta");
-            console.log("te recomiendo un Ryzen 7 o Intel Core I7 y una grafica gama media/alta");
-
-        } else if (this.tipo = "edicion" && this.precio < 100000){
-            alert("te recomiendo seguir ahorrando");
-            console.log("te recomiendo seguir ahorrando");
-
-        } else if (this.tipo = "edicion" && this.precio > 100000 && this.precio < 150000){
-            alert("te recomiendo un Ryzen 5 o Intel Core I5 con una grafica con 6 o 4 GB de vram");
-            console.log("te recomiendo un Ryzen 5 o Intel Core I5 con una grafica con 6 o 4 GB de vra");
-
-        } else if (this.tipo = "edicion" && this.precio > 150000 && this.precio < 200000){
-            alert("te recomiendo un Ryzen 5 o Intel Core I5 con una grafica con 6 o 8 GB de vram");
-            console.log("te recomiendo un Ryzen 5 o Intel Core I5 con una grafica con 6 o 8 GB de vram");
-
-        } else if (this.tipo = "edicion" && this.precio > 200000){
-            alert("te recomiendo un Ryzen 7 o Intel Core I7 y una grafica con mas de 8 GB de vram");
-            console.log("te recomiendo un Ryzen 7 o Intel Core I7 y una grafica con mas de 8 GB de vram");
-
-        } else {
-            alert("Datos incorrectos vuelva a interetarlo");
-        }
-    }
+function completarTabla(){
+    listaPcs.forEach(producto => {
+        let fila = document.createElement("tr");
+        fila.innerHTML = `<td>${producto.tipo}</td><td> $${producto.precio}</td><td>${producto.componentes}</td>`
+        cuerpoTabla.appendChild(fila);
+    })
 }
-let tipoPc = prompt("ingrese el tipo de la pc que busca: (gamer o edicion)");
+completarTabla();
 
-let precioPc = prompt("ingrese presupuesto disponible en pesos Argentinos:");
+function filtrarTabla(array){
+    array.forEach(producto => {
+        let fila = document.createElement("tr");
+        fila.innerHTML = `<td>${producto.tipo}</td><td>$${producto.precio}</td><td>${producto.componentes}</td>`
+        cuerpoTabla.appendChild(fila);
+    })
+}
 
-let Recomendacion = new Computadora(tipoPc, precioPc);
+let elegirPorqueFiltrar = document.querySelectorAll('input[type="radio"]');
 
-Recomendacion.recomendar();
+//elegirPorqueFiltrar.forEach(check => console.log(check));
+
+elegirPorqueFiltrar.forEach(check => check.addEventListener("change", handleChange));
+
+function handleChange () {
+    cuerpoTabla.innerHTML = "";
+
+    const checkeds = Array.from(elegirPorqueFiltrar).filter(check => check.checked);
+    const checkedValue = checkeds.map(check => check.value);
+
+    let arrayFiltrado = [];
+
+    if(checkedValue == "gamer"){
+​
+      arrayFiltrado = listaPcs.filter(producto => producto.tipo == "gamer");
+​
+    }else if (checkedValue == "edicion"){ 
+
+      arrayFiltrado = listaPcs.filter(producto => producto.tipo == "edicon");
+​
+    }else if(checkedValue == "estudiante") {
+
+      arrayFiltrado = listaPcs.filter(producto => producto.tipo == "estudiante");
+​
+    }
+​
+    filtrarTabla(arrayFiltrado);
+}
