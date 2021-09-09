5e4176d5c743a30e703cd0a66bd8921da5993ea4
facundo sarhan <facu.sarhan2000@gmail.com>
2021-09-08 23:54:57 -0300

    js-JQUERY

---
+++ Scripts/back_up.js
@@ -0,0 +1,39 @@
+let cuerpoTabla = document.getElementById("cuerpo_tabla");
+
+function completarTabla(){
+    listaPcs.forEach(producto => {
+        let fila = document.createElement("tr");
+        fila.innerHTML = `<td>${producto.tipo}</td><td> $${producto.precio}</td><td>${producto.componentes}</td>`
+        cuerpoTabla.appendChild(fila);
+    })
+}
+completarTabla();
+
+function filtrarTabla(array){
+    array.forEach(producto => {
+        let fila = document.createElement("tr");
+        fila.innerHTML = `<td>${producto.tipo}</td><td>$${producto.precio}</td><td>${producto.componentes}</td>`
+        cuerpoTabla.appendChild(fila);
+    })
+}
+
+let elegirPorqueFiltrar = document.querySelectorAll('input[type="radio"]');
+
+//elegirPorqueFiltrar.forEach(check => console.log(check));
+
+elegirPorqueFiltrar.forEach(check => check.addEventListener("change", handleChange));
+
+function handleChange () {
+    cuerpoTabla.innerHTML = "";
+    const checkeds = Array.from(elegirPorqueFiltrar).filter(check => check.checked);
+    const checkedValue = checkeds.map(check => check.value);
+    let arrayFiltrado = [];
+    if(checkedValue == "gamer"){
+        arrayFiltrado = listaPcs.filter(producto => producto.tipo == "gamer");
+    }else if (checkedValue == "edicion"){
+        arrayFiltrado = listaPcs.filter(producto => producto.tipo == "edicion");
+    }else if(checkedValue == "estudiante"){
+        arrayFiltrado = listaPcs.filter(producto => producto.tipo == "estudiante");
+    }
+    filtrarTabla(arrayFiltrado);
+}
--- Scripts/recomendaciones.js
+++ Scripts/recomendaciones.js
@@ -1,39 +1,44 @@
-let cuerpoTabla = document.getElementById("cuerpo_tabla");
 
-function completarTabla(){
-    listaPcs.forEach(producto => {
-        let fila = document.createElement("tr");
-        fila.innerHTML = `<td>${producto.tipo}</td><td> $${producto.precio}</td><td>${producto.componentes}</td>`
-        cuerpoTabla.appendChild(fila);
-    })
-}
-completarTabla();
+$(document).ready(function(){
 
-function filtrarTabla(array){
-    array.forEach(producto => {
-        let fila = document.createElement("tr");
-        fila.innerHTML = `<td>${producto.tipo}</td><td>$${producto.precio}</td><td>${producto.componentes}</td>`
-        cuerpoTabla.appendChild(fila);
-    })
-}
-
-let elegirPorqueFiltrar = document.querySelectorAll('input[type="radio"]');
-
-//elegirPorqueFiltrar.forEach(check => console.log(check));
-
-elegirPorqueFiltrar.forEach(check => check.addEventListener("change", handleChange));
-
-function handleChange () {
-    cuerpoTabla.innerHTML = "";
-    const checkeds = Array.from(elegirPorqueFiltrar).filter(check => check.checked);
-    const checkedValue = checkeds.map(check => check.value);
-    let arrayFiltrado = [];
-    if(checkedValue == "gamer"){
-        arrayFiltrado = listaPcs.filter(producto => producto.tipo == "gamer");
-    }else if (checkedValue == "edicion"){
-        arrayFiltrado = listaPcs.filter(producto => producto.tipo == "edicion");
-    }else if(checkedValue == "estudiante"){
-        arrayFiltrado = listaPcs.filter(producto => producto.tipo == "estudiante");
+    /* function completarTabla(){
+        listaPcs.forEach(producto => {
+            let fila = document.createElement("tr");
+            fila.innerHTML = `<td>${producto.tipo}</td><td> $${producto.precio}</td><td>${producto.componentes}</td>`
+            $("#cuerpo_tabla").append(fila);
+        })
     }
-    filtrarTabla(arrayFiltrado);
-}
+    completarTabla(); */
+    
+    function filtrarTabla(array){
+        array.forEach(producto => {
+            let fila = document.createElement("tr");
+            fila.innerHTML = `<td>${producto.tipo}</td><td>$${producto.precio}</td><td>${producto.componentes}</td>`
+            $("#cuerpo_tabla").append(fila);
+        })
+    }
+
+    let elegirPorqueFiltrar = document.querySelectorAll('input[type="radio"]');
+    
+    function recomendaciones () {
+        const checkeds = Array.from(elegirPorqueFiltrar).filter(check => check.checked);
+        const checkedValue = checkeds.map(check => check.value);
+        let arrayFiltrado = [];
+        if(checkedValue == "gamer"){
+            arrayFiltrado = listaPcs.filter(producto => producto.tipo == "gamer");
+        }else if (checkedValue == "edicion"){
+            arrayFiltrado = listaPcs.filter(producto => producto.tipo == "edicion");
+        }else if(checkedValue == "estudiante"){
+            arrayFiltrado = listaPcs.filter(producto => producto.tipo == "estudiante");
+        }
+        filtrarTabla(arrayFiltrado);
+    }
+
+    elegirPorqueFiltrar.forEach(check => check.addEventListener("change", recomendaciones));
+
+    $("#vaciar").on("click", (arrayFiltrado).innerHTML = "");
+});
+
+
+
+
